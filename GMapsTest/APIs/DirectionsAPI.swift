01a0393bf7ac902b0dbac8fd9044d9bb5d66f812
Federico Nieto <federico@lateralview.net>
2018-01-22 12:06:57 -0300

    Directions API improvements

--- GMapsTest/APIs/DirectionsAPI.swift
+++ GMapsTest/APIs/DirectionsAPI.swift
@@ -10,6 +10,22 @@ import UIKit
 import Networking
 import SwiftyJSON
 
+private struct Keys {
+    static let status = "status"
+    static let routes = "routes"
+}
+
+private enum directionsResponseStatus : String {
+    case ok = "OK"
+    case notFound = "NOT_FOUND"
+    case zeroResults = "ZERO_RESULTS"
+    case maxWaypointsExceeded = "MAX_WAYPOINTS_EXCEEDED"
+    case invalidRequest = "INVALID_REQUEST"
+    case overQueryLimit = "OVER_QUERY_LIMIT"
+    case requestDenied = "REQUEST_DENIED"
+    case unknownError = "UNKNOWN_ERROR"
+}
+
 public enum navigationMode : String {
     case walking = "walking"
     case transit = "transit"
@@ -24,21 +40,23 @@ class DirectionsAPI: NSObject {
     
     static let shared = DirectionsAPI()
     
+    
+    //MARK : - Directions
+    
     func getDirections(originLat : Double, originLong: Double, destinationLat: Double, destinationLong: Double, navigationMode: String, completion : @escaping (_ route : Route?, _ error : Error?) -> Void) {
         let networking = Networking(baseURL: baseURL)
         networking.get("maps/api/directions/json?origin=\(originLat),\(originLong)&destination=\(destinationLat),\(destinationLong)&key=\(apiKey)&mode=\(navigationMode)") { result in
             switch result {
             case .success(let response):
                 let responseData = JSON(response.data)
-                let status = responseData["status"].stringValue
-                if status == "OK" {
-                    let routeData = responseData["routes"].arrayValue.first
+                let status = responseData[Keys.status].stringValue
+                if status == directionsResponseStatus.ok.rawValue {
+                    let routeData = responseData[Keys.routes].arrayValue.first
                     let route = Route(data: routeData!)
                     completion(route, nil)
                 } else {
                     completion(nil, nil)
                 }
-                
             case .failure(let response):
                 // Handle error
                 let error = response.error
--- GMapsTest/Model/DirectionsAPI/LineVehicle.swift
+++ GMapsTest/Model/DirectionsAPI/LineVehicle.swift
@@ -9,6 +9,26 @@
 import UIKit
 import SwiftyJSON
 
+public enum VehicleType : String {
+    case rail = "RAIL"
+    case metroRail = "METRO_RAIL"
+    case subway = "SUBWAY"
+    case tram = "TRAM"
+    case monorail = "MONORAIL"
+    case heavyRail = "HEAVY_RAIL"
+    case commuterTrain = "COMMUTER_TRAIN"
+    case highSpeedTrain = "HIGH_SPEED_TRAIN"
+    case bus = "BUS"
+    case intercityBus = "INTERCITY_BUS"
+    case trolleybus = "TROLLEYBUS"
+    case shareTaxi = "SHARE_TAXI"
+    case ferry = "Ferry"
+    case cableCar = "CABLE_CAR"
+    case gondolaLift = "GONDOLA_LIFT"
+    case funicular = "FUNICULAR"
+    case other = "OTHER"
+}
+
 private struct Keys {
     static let name = "name"
     static let type = "type"
@@ -19,13 +39,48 @@ private struct Keys {
 class LineVehicle: NSObject {
 
     var name : String
-    var type : String
+    var type : VehicleType
     var icon : String
     var localIcon : String
     
     init(data: JSON) {
         self.name = data[Keys.name].stringValue
-        self.type = data[Keys.type].stringValue
+        let vehicleType = data[Keys.type].stringValue
+        if vehicleType == VehicleType.rail.rawValue {
+            self.type = .rail
+        } else if vehicleType == VehicleType.bus.rawValue {
+            self.type = .bus
+        } else if vehicleType == VehicleType.cableCar.rawValue {
+            self.type = .cableCar
+        } else if vehicleType == VehicleType.commuterTrain.rawValue {
+            self.type = .commuterTrain
+        } else if vehicleType == VehicleType.ferry.rawValue {
+            self.type = .ferry
+        } else if vehicleType == VehicleType.funicular.rawValue {
+            self.type = .funicular
+        } else if vehicleType == VehicleType.gondolaLift.rawValue {
+            self.type = .gondolaLift
+        } else if vehicleType == VehicleType.heavyRail.rawValue {
+            self.type = .heavyRail
+        } else if vehicleType == VehicleType.highSpeedTrain.rawValue {
+            self.type = .highSpeedTrain
+        } else if vehicleType == VehicleType.intercityBus.rawValue {
+            self.type = .intercityBus
+        } else if vehicleType == VehicleType.metroRail.rawValue {
+            self.type = .metroRail
+        } else if vehicleType == VehicleType.monorail.rawValue {
+            self.type = .monorail
+        } else if vehicleType == VehicleType.subway.rawValue {
+            self.type = .subway
+        } else if vehicleType == VehicleType.tram.rawValue {
+            self.type = .tram
+        } else if vehicleType == VehicleType.trolleybus.rawValue {
+            self.type = .trolleybus
+        } else if vehicleType == VehicleType.shareTaxi.rawValue {
+            self.type = .shareTaxi
+        } else {
+            self.type = .other
+        }
         self.icon = data[Keys.icon].stringValue
         self.localIcon = data[Keys.localIcon].stringValue
     }
